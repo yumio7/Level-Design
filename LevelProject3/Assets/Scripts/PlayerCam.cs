bf5b5d728c73a879b60dda3ff021bed522d007de
jaemin2943 <92981039+jaemin2943@users.noreply.github.com>
2024-02-03 19:18:13 -0500

    Update PlayerCam.cs

--- LevelProject3/Assets/Scripts/PlayerCam.cs
+++ LevelProject3/Assets/Scripts/PlayerCam.cs
@@ -7,7 +7,7 @@ public class PlayerCam : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this script is for player pov
     }
 
     // Update is called once per frame

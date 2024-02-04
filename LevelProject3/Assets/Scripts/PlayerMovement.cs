270aa6f7ec6e5613228eedf0e24f619f3fc86c58
redSoar <132908031+redSoar@users.noreply.github.com>
2024-02-03 19:45:56 -0500

    Created basic player movement

--- LevelProject3/Assets/Scripts/PlayerMovement.cs
+++ LevelProject3/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask ground;
+    bool grounded;
+
+    public Transform direction;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        // words
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        grounded = Physics.Raycast(transform.position, Vector3.down,
+            playerHeight * 0.5f + 0.2f, ground);
+
+        PlayerInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = direction.forward * verticalInput
+            + direction.right * horizontalInput;
+
+        rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }

5e993bd4533a8045e5ff1591ad0fafaa2346beb1
jaemin2943 <92981039+jaemin2943@users.noreply.github.com>
2024-02-28 17:06:50 -0500

    update

---
+++ LevelProject6/Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public float jumpHeight = 2;
+    public float gravity = 60;
+    public float airControl = 10;
+
+    CharacterController controller;
+    Vector3 input, moveDirection;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+
+        input *= moveSpeed;
+
+        if (controller.isGrounded)
+        {
+            //we can jump
+            moveDirection = input;
+            if (Input.GetButton("Jump"))
+            {
+                Debug.Log("Jump Key pressed");
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+            else
+            {
+                moveDirection.y = 0.0f;
+            }
+        }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}

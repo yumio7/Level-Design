98d4f296e5305ed564c2b1614d0ba2911adc773b
Connor Saila <csaila@live.com>
2024-02-29 12:41:24 -0500

    drain functions and prefabs

--- LevelProject6/Assets/scripts/Resources.cs
+++ LevelProject6/Assets/scripts/Resources.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Resources : MonoBehaviour
 {
-    [SerializeField] private int Air = 100;
-    [SerializeField] private int Battery = 100;
+    private float Air;
+    private float Battery;
     [SerializeField] private int AirMax = 100;
     [SerializeField] private int BatteryMax = 100;
 
+    [SerializeField] private GameObject flashlight;
 
-    [SerializeField] private float AirDrain = 0.25f;
-    [SerializeField] private float BatteryDrain = 0.25f;
+    [SerializeField] private float AirDrain = 0.0001f;
+    [SerializeField] private float BatteryDrain = 0.0001f;
 
     private bool AirDraining;
     private bool BatteryDraining;
+    private bool lightout;
 
     private float t;
     private float t2;
@@ -22,24 +25,38 @@ public class Resources : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Air = AirMax;
+        Battery = BatteryMax;
         AirDraining = true;
         BatteryDraining = true;
-        t = AirMax;
-        t2 = BatteryMax;
+        lightout = false;
+        t = 0;
+        t2 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (AirDraining) {
-            Air = (int)Mathf.Lerp(Air, 0, t);
-            t += AirDrain * Time.deltaTime;
+            //Air = (int)Mathf.Lerp(Air, 0, t);
+            //t += AirDrain * 
+            Air -= AirDrain * Time.deltaTime;
         }
         
         if (BatteryDraining) {
-            Battery = (int)Mathf.Lerp(Battery, 0, t2);
-            t2 += BatteryDrain * Time.deltaTime;
+            //Battery = (int)Mathf.Lerp(Battery, 0, t2);
+            //t2 += BatteryDrain * Time.deltaTime;
+            Battery -= BatteryDrain * Time.deltaTime;
         }
+
+        if (Battery < 0)
+            Battery = 0;
+
+        //if (Air <= 0)
+            //GameOver();
+
+            lightout = (Battery <= 0);
+        flashlight.SetActive(!lightout);
     }
 
     public void AddAir(int gain)
@@ -58,4 +75,8 @@ public class Resources : MonoBehaviour
         }
     }
 
+    private void ToggleLight()
+    {
+        flashlight.SetActive(!flashlight.activeSelf);
+    }
 }
